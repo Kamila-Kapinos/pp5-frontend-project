2cbb7fee7cf907481e581ac872922a846b16cc30
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2023-11-11 13:24:47 +0100

    Pierwszy commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kamila-test';
+
+  products?: any[] = [];
+  constructor(private httpClient: HttpClient) {
+  }
+  ngOnInit() {
+    this.httpClient.get<any[]>(
+      '/api/products').subscribe((products) => {
+        console.log({products})
+      this.products = products;
+    })
+
+  }
 }
---
+++ src/proxy.conf.js
@@ -0,0 +1,37 @@
+// {
+//   "/api": {
+//     "target": "http://localhost:9999",
+//     "secure": false
+//   }
+// }
+
+var winston = require('winston'); //(1)
+
+function logProvider() {
+//(2)
+  return winston.createLogger({
+    level: 'debug',
+    format: winston.format.combine(
+      winston.format.splat(),
+      winston.format.simple()
+    ),
+    transports: [new winston.transports.Console()],
+  });
+}
+
+var PROXY_CONF = {
+  '/api': {
+    target: 'http://127.0.0.1:9999',
+    secure: false,
+    changeOrigin: true,
+    logLevel: 'debug',
+    headers: {
+      Connection: 'keep-alive',
+    },
+    // pathRewrite: {
+    //   '^/assets-api': '',
+    // },
+  },
+}
+
+module.exports = PROXY_CONF;
