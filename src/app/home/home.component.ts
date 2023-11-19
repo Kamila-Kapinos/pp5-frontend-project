b2886e5cdafa628060156b5434035f67e2ce89fc
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2023-11-19 17:20:35 +0100

    products page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
-
-  redirectToProductsPage() {
-    this.router.navigate(['/products']);
-  }
-}
+export class HomeComponent {}
