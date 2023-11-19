9ec83676a44234c590cd248e3210f22f5ef3b964
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2023-11-19 17:05:58 +0100

    products page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'Our app';
-
-  products?: any[] = [];
-  constructor(private httpClient: HttpClient) {
-  }
-  ngOnInit() {
-    this.httpClient.get<any[]>(
-      '/api/products').subscribe((products) => {
-        console.log({products})
-      this.products = products;
-    })
-
-  }
+export class AppComponent {
+  title = 'Self Care';
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
-
+export class ProductsComponent implements OnInit {
+  products?: any[] = [];
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit() {
+    this.httpClient.get<any[]>('/api/products').subscribe((products) => {
+      console.log({ products });
+      this.products = products;
+    });
+  }
 }
