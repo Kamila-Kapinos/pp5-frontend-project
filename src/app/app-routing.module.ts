fefd218ffad233003b7acd9d5ceb5bb181cbc917
Kamila-Kapinos <91990616+Kamila-Kapinos@users.noreply.github.com>
2023-11-23 15:41:08 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { CartComponent } from './cart/cart.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
-  { path: 'products', component: ProductsComponent },
   { path: 'cart', component: CartComponent },
 ];
 
