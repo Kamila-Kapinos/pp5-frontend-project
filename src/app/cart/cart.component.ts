3b1ba3920ff04167c53f3544317b67aaf5a14737
JustynaGargula <s221391@student.uek.krakow.pl>
2023-12-11 13:00:41 +0100

    Made cart work, it needs styling

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+
+export class CartComponent implements OnInit {
+  offer: any;
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit() {
+    this.httpClient.get<any[]>('/api/current-offer').subscribe((offer) => {
+      console.log("Current offer: ");
+      console.log({ offer });
+      this.offer = offer;
+    });
+  }
 
 }
