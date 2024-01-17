c817d72cb04b853d7cf8cda200d6255516de2c53
JustynaGargula <s221391@student.uek.krakow.pl>
2024-01-17 11:13:21 +0100

    Worked on client data component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './sales/components/cart/cart.component';
 import { LogInComponent } from './customer/components/log-in/log-in.component';
+import {ClientDataComponent} from "./customer/components/client-data/client-data.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   // { path: 'products', component: ProductsComponent },
   { path: 'cart', component: CartComponent },
   { path: 'logIn', component: LogInComponent },
+  { path: 'clientData', component: ClientDataComponent},
   // {
   //   path: 'details/:id',
   //   component: ProductDetailsComponent,
