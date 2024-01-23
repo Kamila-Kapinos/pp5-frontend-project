b3d28d28628255004f10c1e77e6762c8fdebe8ba
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2024-01-23 12:22:08 +0100

    almost working voucher

--- src/app/sales/components/cart/cart.component.ts
+++ src/app/sales/components/cart/cart.component.ts
@@ -1,36 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { CartProduct } from '../../models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent implements OnInit{
+export class CartComponent implements OnInit {
   cartProducts: CartProduct[] = [];
-
   sum = 0;
   sumQuantity = 0;
   voucherCode = '';
+  voucherApplied = false;
 
-  constructor( private cartService: CartService) {
-  }
+  constructor(private cartService: CartService) {}
 
   ngOnInit() {
     this.cartService.getCart().subscribe((cartProducts) => {
       this.cartProducts = cartProducts;
       this.calc();
     });
-    this.cartService.getProductsQuantity().subscribe(quantity => {
+
+    this.cartService.getProductsQuantity().subscribe((quantity) => {
       this.sumQuantity = quantity;
       this.calc();
-    })
+    });
+
+    this.cartService.getDiscountedPrice().subscribe((discountedSum) => {
+      this.sum = discountedSum;
+      this.calc();
+    });
+
+    this.voucherApplied = this.cartService.isVoucherApplied();
   }
 
   calc() {
     this.sum = this.cartProducts.reduce((prev, curr) => {
-      return prev + (curr.quantity * curr.product.price);
+      return prev + curr.quantity * curr.product.price;
     }, 0);
   }
 
@@ -45,25 +52,29 @@ export class CartComponent implements OnInit{
 
     this.cartService.update({ productId: cartProduct.productId, quantity: cartProduct.quantity });
     this.calc();
-
+    this.applyVoucher();
   }
 
   delete(cartProduct: CartProduct) {
-    this.cartProducts = this.cartProducts.filter(el => el.productId !== cartProduct.productId);
+    this.cartProducts = this.cartProducts.filter((el) => el.productId !== cartProduct.productId);
     this.cartService.delete(cartProduct);
     this.calc();
+    this.applyVoucher();
   }
 
   applyVoucher() {
-    if (this.voucherCode === 'vegan2024' && this.sum > 50) {
-      const discount = this.sum * 0.1;
-      this.sum -= discount;
-
-      console.log('Zastosowano zniżkę 10%');
-    } else {
-      console.log('Nieprawidłowy kod vouchera lub kwota w koszyku za niska');
+    if (this.voucherApplied) {
+      console.log('Voucher already applied');
+      return;
     }
+
+    this.cartService.applyVoucher(this.voucherCode, this.sum).subscribe((result) => {
+      console.log(result);
+
+      if (result.success) {
+        this.sum = result.discountedSum;
+        this.voucherApplied = true;
+      }
+    });
   }
-
-
 }
--- src/app/sales/services/cart.service.ts
+++ src/app/sales/services/cart.service.ts
@@ -1,34 +1,43 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, map, of } from 'rxjs';
-import {CartItem, CartProduct} from '../models/cart.model';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CartItem, CartProduct } from '../models/cart.model';
 import { ProductService } from 'src/app/product/services/product.service';
 
 const CART_SESSION_NAME = 'cart-session-name';
+const DISCOUNTED_SUM_KEY = 'discounted-sum';
+const VOUCHER_CODE_KEY = 'voucher-code';
+const VOUCHER_APPLIED_KEY = 'voucher-applied'; // Nowy klucz dla lokalnego stanu
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-
   private subjectProductsQuantity = new BehaviorSubject(0);
 
   constructor(private productService: ProductService) {
     this.products = this.getSession();
+    this.sum = this.getDiscountedSumFromStorage() || 0;
+    this.voucherCode = this.getVoucherCodeFromStorage() || '';
+    this.voucherApplied = this.getVoucherAppliedFromStorage() || false; // Domyślnie zniżka nie jest jeszcze zastosowana
     this.refreshProductsQuantity();
   }
 
   products: CartItem[] = [];
+  sum = 0;
+  voucherCode = '';
+  voucherApplied = false; // Dodajemy nowe pole
 
   getProductsQuantity(): Observable<number> {
     return this.subjectProductsQuantity.asObservable();
   }
 
   handleAddToCart(productId: string, quantity = 1): boolean {
-    const exist = this.products.find(el => el.productId === productId);
+    const exist = this.products.find((el) => el.productId === productId);
     if (exist) {
       exist.quantity++;
     } else {
-      this.products.push({productId, quantity});
+      this.products.push({ productId, quantity });
     }
     this.refreshSession();
 
@@ -37,20 +46,24 @@ export class CartService {
 
   getCart(): Observable<CartProduct[]> {
     if (this.products.length) {
-      return this.productService.getProducts().pipe(map(products => {
-        return this.products.map(cartItem => {
-          return {
-            ...cartItem,
-            product: products.find(el => el.id === cartItem.productId)
-          }
-        }).filter(el => !!el.product);
-      }))
+      return this.productService.getProducts().pipe(
+        map((products) => {
+          return this.products
+            .map((cartItem) => {
+              return {
+                ...cartItem,
+                product: products.find((el) => el.id === cartItem.productId),
+              };
+            })
+            .filter((el) => !!el.product);
+        })
+      );
     }
     return of([]);
   }
 
   update(cartItem: CartItem) {
-    const index = this.products.findIndex(el => el.productId === cartItem.productId);
+    const index = this.products.findIndex((el) => el.productId === cartItem.productId);
     if (index !== -1) {
       this.products.splice(index, 1, cartItem);
       this.refreshSession();
@@ -58,20 +71,54 @@ export class CartService {
   }
 
   delete(cartItem: CartItem) {
-    const index = this.products.findIndex(el => el.productId === cartItem.productId);
+    const index = this.products.findIndex((el) => el.productId === cartItem.productId);
     if (index !== -1) {
       this.products.splice(index, 1);
       this.refreshSession();
     }
   }
 
-  applyVoucher(voucherCode: string): Observable<any> {
-    return of({ success: true, message: 'Voucher applied successfully.' });
+  applyVoucher(voucherCode: string, sum: number): Observable<any> {
+    if (this.voucherApplied) {
+      console.log('Voucher already applied');
+      return of({ success: false, message: 'Voucher already applied' });
+    }
+
+    this.sum = sum;
+    if (voucherCode === 'vegan2024' && this.sum > 50) {
+      const discount = this.sum * 0.1;
+      this.sum -= discount;
+
+      this.voucherCode = voucherCode;
+      this.voucherApplied = true;
+
+      this.saveDiscountedSumToStorage();
+      this.saveVoucherCodeToStorage();
+      this.saveVoucherAppliedToStorage(); // Zapisz stan zastosowania zniżki
+
+      console.log('Voucher applied successfully. Discount applied: 10%');
+      return of({ success: true, message: 'Voucher applied successfully.', discountedSum: this.sum });
+    } else {
+      console.log('Invalid voucher code or sum too low');
+      return of({ success: false, message: 'Invalid voucher code or sum too low' });
+    }
+  }
+
+  getDiscountedPrice(): Observable<number> {
+    const discountedSum = this.getDiscountedSumFromStorage();
+    return of(discountedSum || 0);
+  }
+
+  isVoucherApplied(): boolean {
+    return this.voucherApplied;
   }
 
   private refreshSession() {
     this.saveSession();
     this.refreshProductsQuantity();
+    this.saveDiscountedSumToStorage();
+    this.saveVoucherCodeToStorage();
+    this.saveVoucherAppliedToStorage();
   }
 
   private refreshProductsQuantity() {
@@ -98,5 +145,31 @@ export class CartService {
       }
     }
     return [];
+  }
+
+  private saveDiscountedSumToStorage() {
+    sessionStorage.setItem(DISCOUNTED_SUM_KEY, JSON.stringify(this.sum));
+  }
+
+  private getDiscountedSumFromStorage() {
+    const discountedSum = localStorage.getItem(DISCOUNTED_SUM_KEY);
+    return discountedSum ? JSON.parse(discountedSum) : null;
+  }
+
+  private saveVoucherCodeToStorage() {
+    sessionStorage.setItem(VOUCHER_CODE_KEY, this.voucherCode);
+  }
+
+  private getVoucherCodeFromStorage() {
+    return sessionStorage.getItem(VOUCHER_CODE_KEY) || '';
+  }
+
+  private saveVoucherAppliedToStorage() {
+    sessionStorage.setItem(VOUCHER_APPLIED_KEY, JSON.stringify(this.voucherApplied));
+  }
+
+  private getVoucherAppliedFromStorage() {
+    const voucherApplied = sessionStorage.getItem(VOUCHER_APPLIED_KEY);
+    return voucherApplied ? JSON.parse(voucherApplied) : null;
   }
 }
