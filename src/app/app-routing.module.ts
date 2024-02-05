ae07234577cd4250798ce8f558014432a6d6c372
JustynaGargula <s221391@student.uek.krakow.pl>
2024-02-05 10:21:41 +0100

    Created shipping and payment form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LogInComponent } from './customer/components/log-in/log-in.component';
 import { ClientDataComponent } from './customer/components/client-data/client-data.component';
+import {ShippingComponent} from "./customer/components/shipping/shipping.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'logIn', component: LogInComponent },
   { path: 'clientData', component: ClientDataComponent },
+  { path: 'shipping', component: ShippingComponent},
   {
     path: 'products',
     loadChildren: () =>
