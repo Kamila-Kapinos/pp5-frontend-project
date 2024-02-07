3466a48092d574a97289f2a18d874cb2eda45558
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2024-02-07 13:59:19 +0100

    aaaaaa

--- src/app/sales/components/summary/summary.component.ts
+++ src/app/sales/components/summary/summary.component.ts
@@ -4,9 +4,7 @@ import { SummaryService } from '../../services/summary.service';
 import { Customer } from '../../models/customer';
 import { CartProduct } from '../../models/cart.model';
 import { CartService } from '../../services/cart.service';
-import {OfferAcceptanceRequest} from "../../models/offer-acceptance-request";
-import {HttpClient} from "@angular/common/http";
-import {ReservationDetails} from "../../models/reservation-details";
+import { OfferAcceptanceRequest } from '../../models/offer-acceptance-request';
 
 @Component({
   selector: 'app-summary',
@@ -16,7 +14,6 @@ import {ReservationDetails} from "../../models/reservation-details";
 export class SummaryComponent implements OnInit {
   offerAcceptanceRequest: OfferAcceptanceRequest = new OfferAcceptanceRequest();
   modalRef?: BsModalRef;
-  products;
   cartProducts: CartProduct[] = [];
   discount = 0;
   sumWithDiscount = 0;
@@ -38,31 +35,26 @@ export class SummaryComponent implements OnInit {
     private modalService: BsModalService,
     private summaryService: SummaryService,
     private cartService: CartService,
-    private http: HttpClient,
-  ) {
-    this.products = summaryService.getCartProducts();
-    this.customer.city = summaryService.getCity();
-    this.customer.email = summaryService.getEmail();
-    this.customer.houseNumber = summaryService.getHouseNumber();
-    this.customer.lastName = summaryService.getLastName();
-    this.customer.name = summaryService.getName();
-    this.customer.phone = summaryService.getPhone();
-    this.customer.postcode = summaryService.getPostcode();
-    this.customer.street = summaryService.getStreet();
-    this.shippingMethod = summaryService.getShippingMethod();
-    this.paymentMethod = summaryService.getPaymentMethod();
-    this.offerAcceptanceRequest.firstname = this.customer.name;
-    this.offerAcceptanceRequest.lastname = this.customer.lastName;
-    this.offerAcceptanceRequest.email = this.customer.email;
-  }
+  ) {}
 
   ngOnInit() {
+    this.summaryService.observeClientData().subscribe((clientData) => {
+      this.customer = clientData;
+      console.log(this.customer);
+    });
+
+    this.summaryService
+      .observeShippingAndPaymentData()
+      .subscribe((shippingAndPaymentData) => {
+        this.shippingMethod = shippingAndPaymentData.shippingMethod;
+        this.paymentMethod = shippingAndPaymentData.paymentMethod;
+      });
+
     this.cartService.getCart().subscribe((cartProducts) => {
       this.cartProducts = cartProducts;
       this.calc();
     });
   }
-
 
   calc() {
     this.sum = this.cartProducts.reduce((prev, curr) => {
@@ -86,11 +78,18 @@ export class SummaryComponent implements OnInit {
   openModal(template: TemplateRef<void>) {
     this.modalRef = this.modalService.show(template);
   }
-  acceptOffer(template: TemplateRef<void>) {
-    this.openModal(template)
 
-    this.http.post<ReservationDetails>('/api/accept-offer', this.offerAcceptanceRequest)
-      .subscribe(reservationDetails => {
+  acceptOffer(template: TemplateRef<void>) {
+    this.openModal(template);
+
+    this.offerAcceptanceRequest.firstname = this.customer.name;
+    this.offerAcceptanceRequest.lastname = this.customer.lastName;
+    this.offerAcceptanceRequest.email = this.customer.email;
+    console.log(this.offerAcceptanceRequest.email);
+
+    this.summaryService
+      .acceptOffer(this.offerAcceptanceRequest)
+      .subscribe((reservationDetails) => {
         console.log('Reservation ID:', reservationDetails.reservationId);
         console.log('Payment URL:', reservationDetails.paymentUrl);
       });
--- src/app/sales/services/summary.service.ts
+++ src/app/sales/services/summary.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Customer } from '../models/customer';
+import { ShippingPayment } from '../models/shipping-payment.model';
+import { ReservationDetails } from '../models/reservation-details';
+import { OfferAcceptanceRequest } from '../models/offer-acceptance-request';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,11 +14,26 @@ export class SummaryService {
   private clientData;
   private shippingData;
 
-  constructor() {
+  private clientDataSubject: BehaviorSubject<Customer>;
+  private shippingDataSubject: BehaviorSubject<ShippingPayment>;
+
+  constructor(private http: HttpClient) {
     this.cartProductsData = sessionStorage.getItem('cart-session-name');
     this.clientData = sessionStorage.getItem('client-session-name');
     this.shippingData = sessionStorage.getItem('shipping-method');
+
+    // Inicjalizacja BehaviorSubjects
+    this.clientDataSubject = new BehaviorSubject(this.getClientData());
+    this.shippingDataSubject = new BehaviorSubject(
+      this.getShippingAndPaymentData(),
+    );
+
+    window.addEventListener('storage', (event) => {
+      this.updateClientDataFromSession(event);
+      this.updateShippingDataFromSession(event);
+    });
   }
+
   getCartProducts() {
     return this.cartProductsData ? JSON.parse(this.cartProductsData) : null;
   }
@@ -20,6 +41,17 @@ export class SummaryService {
   getClientData() {
     return this.clientData ? JSON.parse(this.clientData) : null;
   }
+
+  // Metoda do zwracania obserwowalnego klienta
+  observeClientData() {
+    return this.clientDataSubject.asObservable();
+  }
+
+  // Metoda do zwracania obserwowalnego danych dotyczących wysyłki i płatności
+  observeShippingAndPaymentData() {
+    return this.shippingDataSubject.asObservable();
+  }
+
   getCity() {
     return this.getClientData().city || null;
   }
@@ -62,5 +94,33 @@ export class SummaryService {
 
   getPaymentMethod() {
     return this.getShippingAndPaymentData().paymentMethod || null;
+  }
+
+  private updateClientDataFromSession(event: StorageEvent) {
+    if (event.key === 'client-session-name') {
+      this.clientData = event.newValue;
+      console.log('Aktualizacja danych klienta:', this.clientData);
+      this.clientDataSubject.next(this.getClientData());
+    }
+  }
+
+  private updateShippingDataFromSession(event: StorageEvent) {
+    if (event.key === 'shipping-method') {
+      this.shippingData = event.newValue;
+      console.log(
+        'Aktualizacja danych dostawy i płatności:',
+        this.shippingData,
+      );
+      this.shippingDataSubject.next(this.getShippingAndPaymentData());
+    }
+  }
+
+  acceptOffer(
+    offerAcceptanceRequest: OfferAcceptanceRequest,
+  ): Observable<ReservationDetails> {
+    return this.http.post<ReservationDetails>(
+      '/api/accept-offer',
+      offerAcceptanceRequest,
+    );
   }
 }
