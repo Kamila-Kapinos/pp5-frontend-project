1a6528f7aaed3e928068e5a233f81aa8d8090392
JustynaGargula <s221391@student.uek.krakow.pl>
2024-02-08 13:53:42 +0100

    Added "Add to cart" button in oil, candle and soap components

--- src/app/product/components/candles/candles.component.ts
+++ src/app/product/components/candles/candles.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
+import {CartService} from "../../../sales/services/cart-service/cart.service";
 
 @Component({
   selector: 'app-candles',
@@ -10,7 +11,7 @@ import { ProductService } from '../../services/product.service';
 export class CandlesComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private cartService: CartService,) {}
 
   ngOnInit() {
     this.productService.getProducts().subscribe((products: Product[]) => {
@@ -18,4 +19,20 @@ export class CandlesComponent implements OnInit {
       this.products = products;
     });
   }
+
+  initializeAddToCartHandler(
+    productId: string | null,
+    productName: string,
+  ): void {
+    if (productId !== null) {
+      if (this.cartService.handleAddToCart(productId, productName)) {
+        console.log('Added to cart');
+      } else {
+        console.error('Błąd podczas dodawania do koszyka');
+      }
+    } else {
+      console.error('Błąd: Brak identyfikatora produktu');
+    }
+  }
+
 }
--- src/app/product/components/soaps/soaps.component.ts
+++ src/app/product/components/soaps/soaps.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
+import {CartService} from "../../../sales/services/cart-service/cart.service";
 
 @Component({
   selector: 'app-soaps',
@@ -10,12 +11,27 @@ import { ProductService } from '../../services/product.service';
 export class SoapsComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private cartService: CartService,) {}
 
   ngOnInit() {
     this.productService.getProducts().subscribe((products: Product[]) => {
       console.log({ products });
       this.products = products;
     });
+  }
+
+  initializeAddToCartHandler(
+    productId: string | null,
+    productName: string,
+  ): void {
+    if (productId !== null) {
+      if (this.cartService.handleAddToCart(productId, productName)) {
+        console.log('Added to cart');
+      } else {
+        console.error('Błąd podczas dodawania do koszyka');
+      }
+    } else {
+      console.error('Błąd: Brak identyfikatora produktu');
+    }
   }
 }
